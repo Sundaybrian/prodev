1b35955c50fbb08ab6ab6df8be0cda49565c3b20
Sunday Brian <brians931@gmail.com>
2019-09-07 12:52:24 +0300

    add dummy data to the home page

--- awards/views.py
+++ awards/views.py
@@ -2,5 +2,34 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+# dummy data
+
+sites=[
+    {
+       'dev':'Sunday Brian',
+       'title':'Anitas Kitchen',
+       'date_posted':'September 07,2019',
+       'description':'lorem ipsum lorem ipsum',
+       'link':'https://sundaybrian.github.io/anitas-kitchen-v2/' 
+    },
+        {
+       'dev':'Sunday Priest',
+       'title':'Birthday Version Lady',
+       'date_posted':'September 07,2019',
+       'description':'lorem ipsum lorem ipsum',
+       'link':'https://sundaybrian.github.io/anitas-kitchen-v2/' 
+    },
+        {
+       'dev':'Sunday Omwami',
+       'title':'Anthem',
+       'date_posted':'September 11,2019',
+       'description':'lorem ipsum lorem ipsum',
+       'link':'https://sundaybrian.github.io/anitas-kitchen-v2/' 
+    },
+
+]
 def home(request):
-    return render(request,'awards/home.html')
+    context={
+        'sites':sites
+    }
+    return render(request,'awards/home.html',context)
