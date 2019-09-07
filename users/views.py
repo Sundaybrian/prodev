2131f9bd18c6c840d34ff25998ddc675de91f935
Sunday Brian <brians931@gmail.com>
2019-09-07 15:20:02 +0300

    add the user registration form that add an email field to the usercreation form

--- users/views.py
+++ users/views.py
@@ -13,6 +13,7 @@ def register(request):
          form=UserCreationForm(request.POST)
 
          if form.is_valid():
+             form.save()
              username=form.cleaned_data.get('username')
              messages.success(request,f'Account for {username} created!')
              return redirect('awards-home')
