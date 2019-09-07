1a5c9dca34abb3e4892e667a2d5b13c9a80b6424
Sunday Brian <brians931@gmail.com>
2019-09-07 19:23:20 +0300

    add the profile model

--- awards/models.py
+++ awards/models.py
@@ -28,12 +28,19 @@ class Post(models.Model):
         '''
         self.save()
 
-# class Profile(models.Model):
-#     '''
-#     '''
-#     user=models.OneToOneField(User,on_delete=models.CASCADE)
-#     bio=models.TextField(max_length=140,blank=True)
-#     profile_photo=models.ImageField(upload_to='profile_pics',default='default_profile.png')
+class Profile(models.Model):
+    '''
+    '''
+    user=models.OneToOneField(User,on_delete=models.CASCADE)
+    bio=models.TextField(max_length=140,blank=True)
+    profile_photo=models.ImageField(upload_to='profile_pics',default='default_profile.png')
+    
+    def __str__(self):
+        return f'{self.user.user}-Profile'
+
+    
+        
+
 
 
 
