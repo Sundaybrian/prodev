cf32ccd75ec0412741839752fa50265bf70e21ef
Sunday Brian <brians931@gmail.com>
2019-09-07 15:14:35 +0300

    updated the register form to save a user

--- users/views.py
+++ users/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+
 
 
 # Create your views here.
@@ -7,7 +9,17 @@ def register(request):
     '''
     view function for registering 
     '''
-    form=UserCreationForm()
-    return render(request,'users/registration.html',{'form':form})
+    if request.method=='POST':
+         form=UserCreationForm(request.POST)
+
+         if form.is_valid():
+             username=form.cleaned_data.get('username')
+             messages.success(request,f'Account for {username} created!')
+             return redirect('awards-home')
+
+    else:
+        form=UserCreationForm()
     
+    return render(request,'users/registration.html',{'form':form})
+
 
