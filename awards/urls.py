fdb55a2f0bd5740bdd9f9f4b343e38b10c1f50ff
Sunday Brian <brians931@gmail.com>
2019-09-08 00:46:31 +0300

    add the url for the postupdate class view while also reusing the same template to post and update a post

--- awards/urls.py
+++ awards/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import (PostCreateView)
+from .views import (PostCreateView,PostUpdateView)
 from . import views
 
 urlpatterns=[
     path('',views.home,name='awards-home'),
     path('post/<int:pk>/',views.postDetail,name='post-detail'),
-    path('post/new/',PostCreateView.as_view(),name='post-new')
+    path('post/new/',PostCreateView.as_view(),name='post-new'),
+    path('post/<int:pk>/update/',PostUpdateView.as_view(),name='post-update'),
 ]
