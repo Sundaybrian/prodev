1e499f4c756932d02f93881876b3ec43a71b2caf
Sunday Brian <brians931@gmail.com>
2019-09-07 13:33:20 +0300

    add the post model to submit a website

--- awards/models.py
+++ awards/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.db.models import Q
+from django.core.exceptions import ObjectDoesNotExist
+from django.http import Http404
 
 # Create your models here.
+
+class Post(models.Model):
+    '''
+    
+    '''
+    title=models.CharField(max_length=30)
+    description=models.TextField()
+    link=models.CharField()
+    image=models.ImageField(upload_to='poster/',default='')
+    date_posted=models.DateTimeField(auto_now_add=True)
+    author=models.ForeignKey(User,on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return f'Post{self.title}--{self.description}--{self.author.username}'
+        
