1765c0e12ada3e55cb745edc3a633cf6c842bc3e
Sunday Brian <brians931@gmail.com>
2019-09-08 00:10:25 +0300

    create the url for a post creation

--- awards/urls.py
+++ awards/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns=[
     path('',views.home,name='awards-home'),
     path('post/<int:pk>/',views.postDetail,name='post-detail'),
+    path('post/new/',views.PostCreateView,name='post-new')
 ]
