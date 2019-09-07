655da847ad97d201e0702d98a3c73bc0b404c138
Sunday Brian <brians931@gmail.com>
2019-09-07 15:25:35 +0300

    fix the typo fields in forms.py

--- users/forms.py
+++ users/forms.py
@@ -7,6 +7,6 @@ class UserRegistrationForm(UserCreationForm):
 
     class Meta:
         model=User
-        field=['username','email','password1','password2']
-        
+        fields=['username','email','password1','password2']
+
 
