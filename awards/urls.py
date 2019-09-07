8202bfb9a9c8a176940ee6e4aaf24ef8edeaf0ba
Sunday Brian <brians931@gmail.com>
2019-09-08 01:07:30 +0300

    add post confirm delete html page

--- awards/urls.py
+++ awards/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import (PostCreateView,PostUpdateView)
+from .views import (PostCreateView,PostUpdateView,PostDeleteView)
 from . import views
 
 urlpatterns=[
@@ -7,4 +7,5 @@ urlpatterns=[
     path('post/<int:pk>/',views.postDetail,name='post-detail'),
     path('post/new/',PostCreateView.as_view(),name='post-new'),
     path('post/<int:pk>/update/',PostUpdateView.as_view(),name='post-update'),
+    path('post/<int:pk>/delete/',PostDeleteView.as_view(),name='post-delete'),
 ]
