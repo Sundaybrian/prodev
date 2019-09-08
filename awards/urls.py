ce92a7b336b5f7b00edd41f7e2b400e43818749f
Sunday Brian <brians931@gmail.com>
2019-09-08 12:45:01 +0300

    add url for user post list view

--- awards/urls.py
+++ awards/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import (PostCreateView,PostUpdateView,PostDeleteView,HomeListView)
+from .views import (PostCreateView,PostUpdateView,PostDeleteView,HomeListView,UserPostListView)
 from . import views
 
 urlpatterns=[
     path('',HomeListView.as_view(),name='awards-home'),
+    path('user/<str:username>/posts',UserPostListView.as_view(),name='user-posts'),
     path('post/<int:pk>/',views.postDetail,name='post-detail'),
     path('post/new/',PostCreateView.as_view(),name='post-new'),
     path('post/<int:pk>/update/',PostUpdateView.as_view(),name='post-update'),
